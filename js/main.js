9bd11cc942863ec727177a6cba833f7681146e12
James Williams <jamesphwilliams@gmail.com>
2018-09-30 21:48:58 +0100

    Add code to validate format string input

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
+function getInputRegex() {
+	let formatString = $("#format-input").val();
+
+	let validator = /^(?:[0-9]+[,-]?)*$/gm;
+	if (validator.exec(formatString)) {
+		console.log("Valid");
+	} else {
+		console.log("Invalid");
+	}
+}
+
 function reloadResults() {
+
+	getInputRegex();
+	return;
+
+
 	jQuery.get('sayings.txt', function(data) {
     	for (line of data.split("\n")) {
     		$("#results").append("<tr><td>" + line + "</td></tr>");
