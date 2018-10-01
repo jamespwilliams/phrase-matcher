57d3133e262817be0209a772822fde2afe93a5f2
James Williams <jamesphwilliams@gmail.com>
2018-10-01 19:07:51 +0100

    Add code to display errors to the user when format strings are validated

--- js/main.js
+++ js/main.js
@@ -1,19 +1,39 @@
-function getInputRegex() {
-	let formatString = $("#format-input").val();
+function displayError(message) {
+	$("#format-error").text(message);
+}
 
+function clearErrors() {
+	displayError("");
+}
+
+/*
+	Transforms a crossword style format to a regex
+	e.g: 3,3 -> /^... ...$/
+*/
+function getFormatRegex(formatString) {
 	let validator = /^(?:[0-9]+[,-]?)*$/gm;
-	if (validator.exec(formatString)) {
-		console.log("Valid");
+
+	if (!validator.exec(formatString)) {
+		return null;
 	} else {
-		console.log("Invalid");
+		return /^..............$/;
 	}
 }
 
 function reloadResults() {
+	clearErrors();
 
-	getInputRegex();
+	let formatString = $("#format-input").val();
+
+	let formatRegex = getFormatRegex(formatString);
+
+	console.log(formatRegex);
+
+	if (formatRegex == null) {
+		displayError("Invalid format");
+	}
+
 	return;
-
 
 	jQuery.get('sayings.txt', function(data) {
     	for (line of data.split("\n")) {
