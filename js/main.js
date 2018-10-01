869ee3bc99593977c70a07e6b9278576bd4de902
James Williams <jamesphwilliams@gmail.com>
2018-10-01 21:13:05 +0100

    Add support for wildcards and arbritary characters, and explain this

--- js/main.js
+++ js/main.js
@@ -13,9 +13,9 @@ function clearErrors() {
 function getFormatRegex(formatString) {
 	let validator = /^(?:[0-9]+[,-]?)*$/gm;
 
-	if (!validator.exec(formatString)) {
+	/*if (!validator.exec(formatString)) {
 		return null;
-	}
+	}*/
 
 	let regexString = "^";
 
@@ -23,11 +23,17 @@ function getFormatRegex(formatString) {
 		if (character == ',') {
 			regexString += ' ';
 			continue;
+		} else if (character == '*') {
+			regexString += '.';
+			continue;
 		}
 
 		let num = parseInt(character);
 		if (!isNaN(num)) {
 			regexString += "[a-zA-Z]{" + num + "}";
+		} else {
+			// Just append the character
+			regexString += character;
 		}
 	}
 
@@ -73,3 +79,18 @@ function loadPlaceholderResults() {
 
 $(document).ready(loadPlaceholderResults);
 $("#format-go").on('click', reloadResults);
+
+let isHidden = true;
+
+$("#examples").hide();
+
+$("#show-examples").click(function() {
+	isHidden = !isHidden;
+	if (isHidden) {
+		$("#examples").fadeOut();
+		$("#show-examples").text("Show More Information");
+	} else {
+		$("#examples").fadeIn();
+		$("#show-examples").text("Hide Information");
+	}
+});
